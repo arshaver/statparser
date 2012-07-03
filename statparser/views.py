09dbb05d2d2fdfb8f9d6bc919fe3b2747ed0f129
Anthony Shaver <anthony.r.shaver@gmail.com>
2012-07-02 21:43:30 -0700

    first commit

---
+++ statparser/views.py
@@ -0,0 +1,77 @@
+import csv
+import datetime
+from django.http import HttpResponse
+from statparser.forms import UploadFileForm
+from django.shortcuts import render
+
+INTERVENTIONS = ["ABX","AMINO","ANTICOAG","CARBA","CODE","DIGOXIN","DRUGS","ELECTROLYT","INSULIN","LITHIUM","MISC","PAIN","PHENOBARB","PHENYTOIN","PRADAXA","SENTOUT","THEOPHYL","TPN","VALPROIC","VANCO","WARFARIN","OTHER"]
+
+def parse_stats(file):
+	lines = file.readlines()
+	data = {}
+	
+	for line in lines:
+		if "DATE" in line:
+			date_string = line.split(" ")[1]
+			date_list = date_string.split("/")
+			month = int(date_list[0])
+			day = int(date_list[1])
+			year = int("20"+date_list[2])
+			date = datetime.date(month=month, day=day, year=year)
+			data[date] = {}
+		if any(intervention in line for intervention in INTERVENTIONS):
+			split_line = line.split(" ")
+			split_line = filter(None,split_line)
+
+			intervention = split_line[0]
+			count = split_line[1]
+			data[date][intervention] = int(count)
+
+	#if an intervention from INTERVENTIONS wasn't done on that day, add an entry to say so
+	for intervention in INTERVENTIONS:
+		for date in data:
+			try:
+				data[date][intervention]
+			except KeyError:
+				data[date][intervention] = 0
+	
+	return data
+	
+	
+def main(request):
+	if request.method == 'POST':
+		form = UploadFileForm(request.POST, request.FILES)
+		if form.is_valid():
+			data = parse_stats(request.FILES['file'])
+			
+			response = HttpResponse(mimetype='text/csv')
+			response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
+
+			writer = csv.writer(response)
+			
+			dates = data.keys()
+			dates.sort()
+			writer.writerow([" "]+dates)
+
+			for intervention in INTERVENTIONS:
+				row = []
+				row.append(intervention)
+				for date in dates:
+					row.append(data[date][intervention])
+				writer.writerow(row)
+			return response
+	else:
+		form = UploadFileForm()
+	return render(request, 'upload.html', {'form': form})
+
+
+	
+	# # Create the HttpResponse object with the appropriate CSV header.
+	#	  response = HttpResponse(mimetype='text/csv')
+	#	  response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
+	# 
+	#	  writer = csv.writer(response)
+	#	  writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+	#	  writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+	# 
+	#	  return response
